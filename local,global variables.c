e74e51ea6b4418ecb2a01e63e11e68ec5ad3091a
bhavanasatyasripulum-ux <bhavanasatyasripulum@gmail.com>
2025-11-28 13:35:31 +0530

    Add files via upload

---
+++ local,global variables.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int a=10,b=20;
+void add()
+{
+	int c;
+	c=a+b;
+	printf("%d\n",c);
+}
+void sub()
+{
+	int d;
+	d=a-b;
+	printf("%d\n",d);
+}
+int main()
+{
+	printf("a=%d b=%d\n",a,b);
+	add();
+	sub();
+return 0;
+}
+
