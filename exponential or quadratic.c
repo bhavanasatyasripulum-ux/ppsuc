b5c0e4b45613ca31bc3f8d3162a4a6e1c942badd
bhavanasatyasripulum-ux <bhavanasatyasripulum@gmail.com>
2025-11-28 18:44:11 +0530

    Add files via upload

---
+++ exponential or quadratic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    long long n;
+    scanf("%lld", &n);
+
+    if (n == 1 || n >= 5)
+        printf("Yes
+");
+    else
+        printf("No
+");
+
+    return 0;
+}
+
